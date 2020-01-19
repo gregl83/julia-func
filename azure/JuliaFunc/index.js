2da388bd9e0327df6c48681335a0ca01a180c277
Gregory Langlais <general@gregorylanglais.com>
2020-01-18 21:00:53 -0500

    add azure durable http julia function

---
+++ azure/JuliaFunc/index.js
@@ -0,0 +1,20 @@
+const { spawn } = require('child_process');
+
+module.exports = function (context, req) {
+    const script = req.query.script;
+
+    console.log(`Executing julia script named ${script}`);
+
+    // todo - container script pathing
+
+    const process = spawn(`julia ${script}`);
+
+    process.stdout.on('data', console.log);
+    process.stderr.on('data', console.error);
+
+    process.on('close', (code) => {
+        let body = `child process exited with code ${code}`;
+        context.res = {body: body};
+        context.done(body);
+    });
+};
